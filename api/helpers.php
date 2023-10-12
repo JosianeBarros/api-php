b02019d6a70e17277a147799dcd683bfcfbf6c80
heitorcto <heitorcasella@gmail.com>
2023-10-12 15:09:20 -0300

    refactor: adiciona helper para comunicação com arquivo .env e refatora função de response json

--- api/helpers.php
+++ api/helpers.php
@@ -1,10 +1,30 @@
 <?php
 
-function responseJson(string|array $message, int $httpCode)
+/**
+ * Função responsável por efetuar o retorno da api em formato JSON.
+ *
+ * @param string|array $message
+ * @param integer $httpCode
+ * @return void
+ */
+function responseJson(string|array $message = '', int $httpCode = 200): void
 {
     header("Content-Type: application/json; charset=UTF-8");
 
     http_response_code($httpCode);
 
-    echo json_encode(['mensagem' => $message]);
+    if ($message) {
+        echo json_encode(['mensagem' => $message]);
+    }
+}
+
+/**
+ * Função responsável por resgatar informações das variáveis de ambiente da aplicação.
+ *
+ * @param string $search
+ * @return string
+ */
+function env(string $search): string  
+{
+    return parse_ini_file('../.env')[$search];
 }
