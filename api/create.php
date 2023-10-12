72d436500fcb848c7ed23588c39937523ac82349
heitorcto <heitorcasella@gmail.com>
2023-10-12 15:07:29 -0300

    refactor: reestrutura rota de criação

--- api/create.php
+++ api/create.php
@@ -1,39 +1,31 @@
 <?php
 
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+include_once '../api/helpers.php';
+include_once '../app/Database.php';
+include_once 'db.php';
 
-$dados = file_get_contents('php://input');
-$dados = json_decode($dados, true);
+header('Access-Control-Allow-Methods: POST');
 
-include "db.php";
+$dados = json_decode(file_get_contents('php://input'), true);
 
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if(!empty($dados['nome']) && !empty($dados['autor']) && !empty($dados['editora']) && !empty($dados['paginas']) && !empty($dados['ano']) && !empty($dados['quantidade'])) {
-        // Conexão com o banco de dados
-        $connection = conectar();
-
-        $nome = $connection->real_escape_string($dados['nome']);
-        $autor = $connection->real_escape_string($dados['autor']);
-        $editora = $connection->real_escape_string($dados['editora']);
-        $paginas = $connection->real_escape_string($dados['paginas']);
-        $ano = $connection->real_escape_string($dados['ano']);
-        $quantidade = $connection->real_escape_string($dados['quantidade']);
-
-        $insertDados = "INSERT INTO livros (nome, autor, editora, paginas, ano_da_publicacao, quantidade) VALUES ('$nome', '$autor', '$editora', '$paginas', '$ano', '$quantidade')";
-
-        if ($connection->query($insertDados) === TRUE) {
-            echo json_encode(["mensagem" => "Livro cadastrado com sucesso"]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!in_array("", $dados)) {
+        $database = new Database;
+        
+        if ($database->insert(
+            "INSERT INTO livros 
+                (nome, autor, editora, paginas, ano_da_publicacao, quantidade) 
+            VALUES 
+                (:nome, :autor, :editora, :paginas, :ano_da_publicacao, :quantidade)",
+            $dados
+        )) {
+            responseJson('Livro cadastrado com sucesso', 201);
         } else {
-            echo json_encode(["mensagem" => "Erro: " . $insertDados . "<br>" . $connection->error]);
+            responseJson(httpCode: 400);
         }
-
-        $connection->close();
     } else {
-        echo json_encode(["mensagem" => "Preencha todos os campos"]);
+        responseJson('Preencha todos os campos', 202);
     }
 } else {
-   echo json_encode(["mensagem" => "Error"]);
+    responseJson(httpCode: 405);
 }
-
-?>
